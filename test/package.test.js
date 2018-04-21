616f4706bb9a182861e1271cbd74060aff7e7a0c
Jared Hanson <jaredhanson@gmail.com>
2018-04-21 10:58:33 -0700

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+var expect = require('chai').expect;
+
+
+describe('@authnomicon/authorize', function() {
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
+  });
+  
+});
