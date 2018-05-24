e2bd52257bd713ed52d3da863e9cd335fa5c5979
Jared Hanson <jaredhanson@gmail.com>
2018-05-24 07:54:36 -0700

    Clarify argument names.

--- app/negotiate/confirmation.js
+++ app/negotiate/confirmation.js
@@ -3,32 +3,32 @@ exports = module.exports = function() {
   var SUPPORTED_METHODS = [ 'none' ];
   
   
-  return function negotiateConfirmation(resources, client, cb) {
-    if (!Array.isArray(resources)) {
-      resources = [ resources ];
+  return function negotiateConfirmation(recipients, presenter, cb) {
+    if (!Array.isArray(recipients)) {
+      recipients = [ recipients ];
     }
     
     // TODO: Initialize types to those supported by AS
     
-    var supportedMethods = resources[0].confirmationMethodsSupported || SUPPORTED_METHODS
-      , resource, i, len;
+    var supportedMethods = recipients[0].confirmationMethodsSupported || SUPPORTED_METHODS
+      , recipient, i, len;
       
     // Filter the array of supported methods to those supported by the resource
     // servers this token will be used to authorize access to.
-    for (i = 1, len = resources.length; i < len; ++i) {
-      resource = resources[i];
-      if (resource.confirmationMethodsSupported) {
+    for (i = 1, len = recipients.length; i < len; ++i) {
+      recipient = recipients[i];
+      if (recipient.confirmationMethodsSupported) {
         supportedMethods = supportedMethods.filter(function(e) {
-          return resource.confirmationMethodsSupported.indexOf(e) !== -1;
+          return recipient.confirmationMethodsSupported.indexOf(e) !== -1;
         });
       }
     }
     
     // Filter the array of supported types to those supported by the client that
     // is requesting access to the resource servers.
-    if (client.confirmationMethodsSupported) {
+    if (presenter.confirmationMethodsSupported) {
       supportedMethods = supportedMethods.filter(function(e) {
-        return client.confirmationMethodsSupported.indexOf(e) !== -1;
+        return presenter.confirmationMethodsSupported.indexOf(e) !== -1;
       });
     }
     
