4ff114a152483e94c23acda893b7342183e4d849
Jared Hanson <jaredhanson@gmail.com>
2018-08-04 12:21:04 -0700

    Remove consent from assembly.

--- test/package.test.js
+++ test/package.test.js
@@ -5,6 +5,16 @@ var expect = require('chai').expect;
 
 describe('@authnomicon/sts', function() {
   
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('org.authnomicon/sts');
+      
+      expect(json.assembly.components).to.have.length(1);
+    });
+  });
+  
   it('should throw if required', function() {
     expect(function() {
       var pkg = require('..');
